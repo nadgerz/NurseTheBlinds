e4e0023fa722ce488ebb4dc11b4bb4be8a49a17c
Steve Ingram <stephen.a.ingram@gmail.com>
2011-02-07 13:18:50 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NurseTheBlinds::Application.config.secret_token = 'd9fe0658fbadf15870347c75e94bbd81d1cb811fae8e7b835de510ee5870103e2256b4e263e45ad1b060512f394102388fa4a9de42160cddc8d19999a2c28472'
